58a0a14bb3bd92f2000dcddda6a5f29b12644cec
Sebastian Paz <pazsebas797@gmail.com>
2021-11-28 03:06:26 -0500

    Final commit

--- src/model/Contries.java
+++ src/model/Contries.java
@@ -1,5 +1,92 @@
 package model;
 
-public class Contries {
+public class Contries implements Comparable<Contries> {
 
+	private String country;
+	private int goldm, silverm, broncem, goldf, silverf, broncef,goldc,silverc,bronzec;
+	
+	public Contries(String country, int goldm, int silverm, int broncem, int goldf, int silverf, int broncef) {
+		this.country = country;
+		this.goldm = goldm;
+		this.silverm = silverm;
+		this.broncem = broncem;
+		this.goldf = goldf;
+		this.silverf = silverf;
+		this.broncef = broncef;
+		goldc=0;
+		silverc=0;
+		bronzec=0;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public int getGoldm() {
+		return goldm;
+	}
+
+	public int getSilverm() {
+		return silverm;
+	}
+
+	public int getBroncem() {
+		return broncem;
+	}
+
+	public int getGoldf() {
+		return goldf;
+	}
+
+	public int getSilverf() {
+		return silverf;
+	}
+
+	public int getBroncef() {
+		return broncef;
+	}
+	
+	public int getGoldc() {
+		return goldc=goldm+goldf;
+	}
+
+	public int getSilverc() {
+		return silverc=silverm+silverf;
+	}
+
+	public int getBronzec() {
+		return bronzec=broncem+broncef;
+	}
+
+	public int compareByGoldw(Contries otherobj) {
+		if(goldf>otherobj.getGoldf()) {
+			return 1;
+		}
+		else if(goldf<otherobj.getGoldf()) {
+			return -1;
+		}
+		else {
+			if(goldf>otherobj.getSilverf()) {
+				return 1;
+			}
+			else if(goldf<otherobj.getSilverf()) {
+				return -1;
+			}
+			else {
+				if(goldf>otherobj.getBroncef()) {
+					return 1;
+				}
+				else if(goldf<otherobj.getBroncef()) {
+					return -1;
+				}
+				else
+					return 0;
+			}
+		}
+	}
+
+	@Override
+	public int compareTo(Contries o) {
+		return 0;
+	}
 }
--- src/model/ControllerModel.java
+++ src/model/ControllerModel.java
@@ -1,5 +1,68 @@
 package model;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import model.GoldandSilever;
+
 public class ControllerModel {
 
+	List<Contries> country;
+	List<Contries>replacement;
+	
+	public ControllerModel() {
+		
+		country=new ArrayList<Contries>();
+	}
+	
+	public void useBufferedReader() throws IOException {
+		
+		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+		int counter=25;
+		
+		while(counter>0) {
+			
+			String line=br.readLine();
+			String[]parts=line.split(";");
+			Contries register= new Contries(parts[0],parts[1],parts[2],parts[3],parts[4],parts[5],parts[6]);
+			addCountry(register);
+			counter--;
+		}
+		br.close();
+	}
+	
+	public void addCountry(Contries newCountry) {
+		
+		country.add(newCountry);
+		replacement.add(newCountry);
+	}
+	
+	public void sortByGoldAndSilverandBronzeMen() {
+		Comparator<Contries>s1= new GoldandSilever();
+		Collections.sort(country,s1);
+	}
+	
+	public void sortByGoldAndSilverandBronzeMan() {
+		int j;
+		Contries aux;
+		
+		for(int i=1;i<country.size();i++) {
+			aux=country.get(i);
+			j=i-1;
+			while(j>=0 && (aux.compareByGoldw(country.get(j))<0)) {
+				country.set(j+1, country.get(j));
+				j--;
+			}
+			country.set(j+1, aux);
+		}
+	}
+	
+	public void bubbleSort() {
+		
+	}
 }
+
